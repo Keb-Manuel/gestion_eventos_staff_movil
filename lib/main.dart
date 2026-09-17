b0de087eab934bea703c48a99c5d0b5654bb1eda
ManuelPech8991 <8991@itescam.edu.mx>
2026-09-16 20:29:55 -0600

    feature: main and app_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'pages/propina_page.dart';
+import 'pages/combustible_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Practica02App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Practica02App extends StatelessWidget {
+  const Practica02App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Práctica 02 - Dart y Estado',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
       ),
+      initialRoute: '/propina',
+      routes: {
+        '/propina': (context) => const PropinaPage(),
+        '/combustible': (context) => const CombustiblePage(),
+      },
     );
   }
 }
