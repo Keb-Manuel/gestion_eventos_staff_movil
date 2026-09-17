988efa7a5e42be5f9434e9ed1072838723ce9d20
jimmyC890 <jimmychan336@gmail.com>
2026-09-16 23:19:41 -0600

    feat: completa practica 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MiApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Práctica 1',
+      theme: ThemeData(useMaterial3: true),
+      home: const InicioPage(),
+    );
+  }
+}
+
+class InicioPage extends StatelessWidget {
+  const InicioPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tópicos de Programación Móvil')),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.phone_android, size: 80),
+            SizedBox(height: 16),
+            Text('¡Hola Flutter!', style: TextStyle(fontSize: 28)),
+            Text('Mi primera aplicación con Dart'),
+          ],
         ),
       ),
     );
