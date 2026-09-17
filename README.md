6b3cfa0e18f89f62816621c230a2ab15c6db2c77
Manuel Orlando <manuelorlandokebortiz@gmail.com>
2026-09-16 19:32:03 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gestion_eventos_staff_movil
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/gestion_eventos_staff_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gestion_eventos_staff_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
