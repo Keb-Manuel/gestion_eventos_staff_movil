418221a93253b715d3189ed0fe027510bf50ee34
Manuel Orlando <manuelorlandokebortiz@gmail.com>
2026-09-25 20:53:09 -0600

    feature: Creacion del codigo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
+import 'screens/inicio_page.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
       ),
-    );
-  }
+      home: const InicioPage(),
+    ),
+  );
 }
