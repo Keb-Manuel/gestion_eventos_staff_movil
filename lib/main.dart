4f98af02a26fa81500acaeefd0d384878e84a01c
Manuel Orlando <manuelorlandokebortiz@gmail.com>
2026-09-25 22:11:06 -0600

    feature: Add initial images and update main app structure with tests

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import 'screens/inicio_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const CatalogoApp());
+}
+
+class CatalogoApp extends StatelessWidget {
+  const CatalogoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Catálogo',
       theme: ThemeData(
         useMaterial3: true,
       ),
       home: const InicioPage(),
-    ),
-  );
+    );
+  }
 }
